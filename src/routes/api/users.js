e8538e748575f87d6fbd3fedee7ba7fad71f1e78
Saul Meneses <saul.meneses@conduent.com>
2019-08-05 14:55:25 -0700

    Validation for registration and login

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,13 +1,27 @@
 const router = require('express').Router({ mergeParams: true });
 const passport = require('passport');
+const Joi = require('@hapi/joi');
+const validate = require('express-joi-validator');
 
 const { UsersController } = require('../../controllers/api');
 
 router.route('/register/')
-  .post(UsersController.registerUser);
+  .post(validate({
+    body: {
+      firstName: Joi.string().min(2).max(255).required(),
+      lastName: Joi.string().min(2).max(255).required(),
+      email: Joi.string().email({ minDomainSegments: 2 }).max(255).required(),
+      password: Joi.string().min(6).max(255).required(),
+    },
+  }), UsersController.registerUser);
 
 router.route('/login/')
-  .post(UsersController.logInUser);
+  .post(validate({
+    body: {
+      email: Joi.string().email({ minDomainSegments: 2 }).max(255).required(),
+      password: Joi.string().max(255).required(),
+    },
+  }), UsersController.logInUser);
 
 router.route('/me/')
   .get(passport.authenticate('jwt', { session: false }), UsersController.myData);
