048bebbbab10741deee7cdeddf0a853bb5939dbf
Saul M <fob2257@gmail.com>
2019-08-22 10:44:31 -0700

    Persist userReducer data if client makes a refresh

--- app/src/App.js
+++ app/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import './App.css';
-
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
@@ -12,8 +10,28 @@ import Register from './components/Register';
 import Login from './components/Login';
 
 import { storeFactory } from './stores';
+import { setAuthToken, decodeAuthToken } from './utils';
+import { logInUserType, logOutUserAction } from './actions/userAction';
+
+import './App.css';
 
 const store = storeFactory();
+
+const token = localStorage.getItem('token');
+if (token) {
+  decodeAuthToken(token)
+    .then((data) => {
+      if (Object.keys(data).length > 0) {
+        setAuthToken(token);
+        return store.dispatch(logInUserType(data));
+      }
+
+      logOutUserAction()(store.dispatch);
+    })
+    .catch(() => {
+      logOutUserAction()(store.dispatch);
+    });
+}
 
 function App() {
   return (
